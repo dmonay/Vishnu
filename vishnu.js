638b08be0296277e5d658cd376201491cd7813a2
Dmitry Pavluk <pavluk73@gmail.com>
2014-02-24 15:24:39 -0500

    v1.0

--- vishnu.js
+++ vishnu.js
@@ -1,31 +1,23 @@
 
-
 var a = 0;
 var b = 0;
 var review1 = "";
 var review2 = "";
+var api = [];
 
 $(document).ready(function () {     
     
-    /*    $.getJSON("http://api.rottentomatoes.com/api/public/v1.0/movies/770672122/reviews.json?review_type=top_critic&page_limit=20&page=1&country=us&apikey=c8vcknsryddyty8xreagauub", function (json) {
-            console.log(json);
-            }); 
-            */
-    
-
-
     var start = function () {
         $("#start").addClass("hidden"); 
         $("#movies").removeClass('hidden').addClass("visible"); 
-        var x = Math.floor(Math.random()*26);
-        var y = Math.floor(Math.random() * (26)) + 25;      
+        var x = Math.floor(Math.random()*21);
+        var y = Math.floor(Math.random() * (19)) + 20;      
         $.ajax({
             type: "GET",
             dataType: 'JSONP',
             crossDomain: 'true',
             url: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=50&country=us&apikey=c8vcknsryddyty8xreagauub",
             success: function (resp) {
-                console.log(resp);
                 var movieA = resp.movies[x].title;
                 var movieB = resp.movies[y].title;
                 var ratingA = resp.movies[x].ratings.critics_score;
@@ -36,6 +28,7 @@ $(document).ready(function () {
                 b = ratingB;
                 review1 = resp.movies[x].critics_consensus;
                 review2 = resp.movies[y].critics_consensus;
+                api = resp;
             },
             error: function (error) {
                 alert(error);
@@ -47,9 +40,6 @@ $(document).ready(function () {
         $("#reviews").removeClass('hidden').addClass("visible");
         $('#movie1_review').html(review1);
         $('#movie2_review').html(review2);
-
-        console.log(review1);
-        console.log(review2);
         $("#next").removeClass('hidden').addClass("visible");
     };
 
@@ -69,30 +59,18 @@ $(document).ready(function () {
         $('#movie2_review').empty();
         $('#reviews').removeClass('visible').addClass('hidden');
         $("#next").removeClass('visible').addClass("hidden");
-        var x = Math.floor(Math.random()*26);
-        var y = Math.floor(Math.random() * (26)) + 25;
-        $.ajax({
-            type: "GET",
-            dataType: 'JSONP',
-            crossDomain: 'true',
-            url: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=50&country=us&apikey=c8vcknsryddyty8xreagauub",
-            success: function (resp) {
-                console.log(resp);
-                var movieA = resp.movies[x].title;
-                var movieB = resp.movies[y].title;
-                var ratingA = resp.movies[x].ratings.critics_score;
-                var ratingB = resp.movies[y].ratings.critics_score;
-                $('#movie1').html(movieA);
-                $('#movie2').html(movieB);
-                a = ratingA;
-                b = ratingB;
-                review1 = resp.movies[x].ratings.critics_consensus;
-                review2 = resp.movies[y].ratings.critics_consensus; 
-            },
-            error: function (error) {
-                alert(error);
-            }
-        });
+        var x = Math.floor(Math.random()*21);
+        var y = Math.floor(Math.random() * (19)) + 20;
+        var movieA = api.movies[x].title;
+        var movieB = api.movies[y].title;
+        var ratingA = api.movies[x].ratings.critics_score;
+        var ratingB = api.movies[y].ratings.critics_score;
+        $('#movie1').html(movieA);
+        $('#movie2').html(movieB);
+        a = ratingA;
+        b = ratingB;
+        review1 = api.movies[x].critics_consensus;
+        review2 = api.movies[x].critics_consensus;         
     };
 
     $('#start').click(start);
